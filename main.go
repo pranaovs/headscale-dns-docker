0cf9855c260b12f9e9fdddb4ee99d05068a1a552
Pranaov S <148376495+pranaovs@users.noreply.github.com>
2025-12-05 00:35:40 +0530

    feat: connect to docker socket using env vars/default

--- main.go
+++ main.go
@@ -1,4 +1,48 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/docker/go-sdk/client"
+)
+
 func main() {
+	ctx := context.Background()
+
+	// Create client with options
+	cli, err := client.New(
+		ctx,
+		getDockerOptions()...,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Cleanup
+	defer func() {
+		err := cli.Close()
+		if err != nil {
+			log.Fatalf("Error closing Docker client: %v", err)
+		}
+	}()
+}
+
+func getDockerOptions() []client.ClientOption {
+	options := []client.ClientOption{}
+
+	dockerHost := os.Getenv("DOCKER_HOST")
+	if dockerHost != "" {
+		log.Printf("Using DOCKER_HOST: %s", dockerHost)
+		options = append(options, client.WithDockerHost(dockerHost))
+	}
+
+	dockerContext := os.Getenv("DOCKER_CONTEXT")
+	if dockerContext != "" {
+		log.Printf("Using DOCKER_CONTEXT: %s", dockerContext)
+		options = append(options, client.WithDockerContext(dockerContext))
+	}
+
+	return options
 }
